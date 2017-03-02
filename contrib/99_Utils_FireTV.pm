e95d23779ba97c7f6a1521829d451d4ae88abedd
nesges <nesges@gmail.com>
2017-03-02 12:00:16 +0100

    added $Id$ 99_Utils_FireTV.pm

--- contrib/99_Utils_FireTV.pm
+++ contrib/99_Utils_FireTV.pm
@@ -5,6 +5,7 @@
 # Please copy this file from contrib/ to FHEM/ and edit where necessary
 #
 # 2017 by Thomas Nesges <thomas@nesges.eu>
+# $Id$
 ########################################################################
 
 package main;
