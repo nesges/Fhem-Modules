33ca04ca39f11d2719291e60b5c1a9d00c32422b
nesges <nesges@gmail.com>
2017-03-09 14:06:32 +0100

    doc2mw.sh: better handling of pre tags

--- doc2mw.sh
+++ doc2mw.sh
@@ -3,4 +3,11 @@ $f=~s/.*?=begin html(.*?)=end html.*/$1/s;
 $f=~s/<ul>(\s*<)(?!li>)/<div>$1/sg; 
 $f=~s/(?<!<\/li)(>\s*)<\/ul>/$1<\/div>/sg; 
 $f=~s/(<a href=.)#/$1https:\/\/fhem.de\/commandref.html#/g;
+$w=$f;
+$w=~s/(<pre>.*?)(?<!<br\/><tt>)[\r\n]+(.*?<\/pre>)/$1<\/tt><br\/><tt>$2/sg; 
+while($w ne $f) {
+    $f=$w;
+    $w=~s/(<pre>.*?)(?<!<br\/><tt>)[\r\n]+(.*?<\/pre>)/$1<\/tt><br\/><tt>$2/sg; 
+}
+$f=~s/<(\/?)pre>/<$1tt>/sg; 
 print $f' $1 | pandoc --from html --to mediawiki | sed -e 's/<code>/<tt>/g' | sed -e 's/<\/code>/<\/tt>/g' | sed -e 's/===/=/g'
